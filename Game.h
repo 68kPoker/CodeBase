a361e19f8feb7416d855b2d46caba277f6675bb5
Robert Szacki <robert.szacki@gmail.com>
2021-02-18 23:47:05 +0100

    Added menu and board editing functions

---
+++ Game.h
@@ -0,0 +1,49 @@
+
+#include <exec/types.h>
+
+#define BOARD_WIDTH  20
+#define BOARD_HEIGHT 16
+
+enum
+{
+    FLOOR_KIND,
+    WALL_KIND,
+    OBJECT_KIND,
+    ITEM_KIND
+};
+
+enum
+{
+    NORMAL_FLOOR,
+    FLAGSTONE_FLOOR
+};
+
+enum
+{
+    NORMAL_WALL,
+    DOOR_WALL
+};
+
+enum
+{
+    BOX_OBJECT,
+    HERO_OBJECT
+};
+
+enum
+{
+    FRUIT_ITEM,
+    KEY_ITEM
+};
+
+struct Cell
+{
+    WORD kind:  4;
+    WORD subKind: 4;
+    WORD floor: 4;
+};
+
+struct Board
+{
+    struct Cell board[BOARD_HEIGHT][BOARD_WIDTH];
+};
