c791318bd46f98136f94f212759cb092aaccc828
Robert Szacki <robert.szacki@gmail.com>
2021-03-09 15:39:59 +0100

    Windows file fix

--- Windows.c
+++ Windows.c
@@ -1,282 +1,62 @@
 
-/* Window.c: Auxilliary window functions */
+#include "Windows.h"
+#include "Gadgets.h"
 
 #include <intuition/intuition.h>
+#include <exec/memory.h>
 
+#include <clib/exec_protos.h>
 #include <clib/intuition_protos.h>
-#include <clib/layers_protos.h>
+#include <clib/alib_protos.h>
+#include <clib/utility_protos.h>
 
-#include "Window.h"
-#include "Tile.h"
-
-struct Window *openBDWindow(struct Screen *s, struct windowInfo *wi)
+struct TagItem *getWindowTags(struct Screen *s)
 {
-    struct Window *w;
-
-    if (w = OpenWindowTags(NULL,
-        WA_CustomScreen,    s,
-        WA_Left,            0,
-        WA_Top,             0,
-        WA_Width,           s->Width,
-        WA_Height,          s->Height,
-        WA_Gadgets,         wi->gads,
-        WA_Backdrop,        TRUE,
-        WA_Borderless,      TRUE,
-        WA_Activate,        TRUE,
-        WA_RMBTrap,         TRUE,
-        WA_BackFill,        LAYERS_NOBACKFILL,
-        WA_SimpleRefresh,   TRUE,
-        WA_IDCMP,           IDCMP_MOUSEBUTTONS|IDCMP_MOUSEMOVE|IDCMP_GADGETUP|IDCMP_REFRESHWINDOW|IDCMP_RAWKEY|IDCMP_INTUITICKS,
-        WA_ReportMouse,     TRUE,
-        TAG_DONE))
+    static struct TagItem tags[] =
     {
-        return(w);
-    }
-    return(NULL);
+        { WA_CustomScreen,  0 },
+        { WA_Left,          0 },
+        { WA_Top,           0 },
+        { WA_Width,         320 },
+        { WA_Height,        256 },
+        { WA_Backdrop,      TRUE },
+        { WA_Borderless,    TRUE },
+        { WA_Activate,      TRUE },
+        { WA_RMBTrap,       TRUE },
+        { WA_IDCMP,         0 },
+        { WA_SimpleRefresh, 0 },
+        { WA_BackFill,      (ULONG)LAYERS_NOBACKFILL },
+        { WA_ReportMouse,   TRUE },
+        { TAG_DONE }
+    };
+
+    tags[0].ti_Data = (ULONG)s;
+
+    return(tags);
 }
 
-struct Window *openMenuWindow(struct Window *p, WORD left, WORD width, WORD height, struct Gadget *gads)
+struct windowInfo *openWindow(struct TagItem *base, ULONG tag1, ...)
 {
-    struct Screen *s = p->WScreen;
-    struct Window *w;
+    struct windowInfo *wi;
 
-    if (w = OpenWindowTags(NULL,
-        WA_CustomScreen,    s,
-        WA_Gadgets,         gads,
-        WA_Left,            left,
-        WA_Top,             240 - height,
-        WA_Width,           width,
-        WA_Height,          height,
-        WA_Borderless,      TRUE,
-        WA_Activate,        TRUE,
-        WA_RMBTrap,         TRUE,
-        WA_BackFill,        LAYERS_NOBACKFILL,
-        WA_SimpleRefresh,   TRUE,
-        WA_IDCMP,           IDCMP_MOUSEBUTTONS|IDCMP_GADGETUP,
-        TAG_DONE))
+    if (wi = AllocMem(sizeof(*wi), MEMF_PUBLIC|MEMF_CLEAR))
     {
-        return(w);
-    }
-    return(NULL);
-}
+        ApplyTagChanges(base, (struct TagItem *)&tag1);
 
-struct Window *openReqWindow(struct Window *p, WORD left, WORD top, WORD width, WORD height, struct Gadget *gads)
-{
-    struct Screen *s = p->WScreen;
-    struct Window *w;
-
-    if (w = OpenWindowTags(NULL,
-        WA_CustomScreen,    s,
-        WA_Left,            left,
-        WA_Top,             top,
-        WA_Width,           width,
-        WA_Height,          height,
-        WA_Borderless,      TRUE,
-        WA_Activate,        TRUE,
-        WA_RMBTrap,         TRUE,
-        WA_BackFill,        LAYERS_NOBACKFILL,
-        WA_SimpleRefresh,   TRUE,
-        WA_IDCMP,           IDCMP_RAWKEY|IDCMP_MOUSEBUTTONS|IDCMP_GADGETUP,
-        TAG_DONE))
-    {
-        return(w);
-    }
-    return(NULL);
-}
-
-/* moveWindow: This proc moves intuition window by dx/dy */
-void moveWindow(struct Window *w, WORD dx, WORD dy)
-{
-    struct Layer *oldl = w->WLayer; /* Get old layer */
-    struct Layer *newl; /* New layer */
-    struct Layer_Info *li = &w->WScreen->LayerInfo;
-    struct BitMap *bm = w->RPort->BitMap;
-    struct Screen *s = w->WScreen;
-
-    struct Rectangle *rect = &oldl->bounds;
-    ULONG lock;
-
-    if ((rect->MinX + dx < 0) || (rect->MaxX + dx >= s->Width) || (rect->MinY + dy < 0) || (rect->MaxY + dy >= s->Height))
-        return;
-
-    lock = LockIBase(0);
-
-    /* Create layer in new position */
-    if (newl = CreateBehindHookLayer(li, bm, rect->MinX + dx, rect->MinY + dy, rect->MaxX + dx, rect->MaxY + dy, LAYERSIMPLE, LAYERS_NOBACKFILL, NULL))
-    {
-        /* Attach new layer */
-        w->WLayer = newl;
-        w->RPort = newl->rp;
-
-        /* Attach window */
-        newl->Window = w;
-
-        /* Change position */
-        w->LeftEdge += dx;
-        w->TopEdge += dy;
-
-        MoveLayerInFrontOf(newl, oldl->back);
-
-        /* Delete old layer */
-        DeleteLayer(0, oldl);
-    }
-
-    UnlockIBase(lock);
-}
-
-BOOL initImages(struct Image img[], struct BitMap *gfx)
-{
-    if (cutImage(img + IMG_BUTTON, gfx, 0, 0, 64, 16))
-    {
-        if (cutImage(img + IMG_PRESSED, gfx, 80, 0, 64, 16))
+        if (wi->window = OpenWindowTagList(NULL, base))
         {
-            if (cutImage(img + IMG_CLOSE, gfx, 0, 16, 16, 16))
-            {
-                if (cutImage(img + IMG_CLOSE_PRESSED, gfx, 16, 16, 16, 16))
-                {
-                    if (cutImage(img + IMG_NEXT, gfx, 64, 16, 16, 16))
-                    {
-                        if (cutImage(img + IMG_NEXT_PRESSED, gfx, 80, 16, 16, 16))
-                        {
-                            if (cutImage(img + IMG_PREV, gfx, 96, 16, 16, 16))
-                            {
-                                if (cutImage(img + IMG_PREV_PRESSED, gfx, 112, 16, 16, 16))
-                                {
-                                    return(TRUE);
-                                }
-                                freeImage(img + IMG_PREV);
-                            }
-                            freeImage(img + IMG_NEXT_PRESSED);
-                        }
-                        freeImage(img + IMG_NEXT);
-                    }
-                    freeImage(img + IMG_CLOSE_PRESSED);
-                }
-                freeImage(img + IMG_CLOSE);
-            }
-            freeImage(img + IMG_PRESSED);
+            NewList((struct List *)&wi->gadList);
+
+            wi->window->UserData = (APTR)wi;
+            return(wi);
         }
-        freeImage(img + IMG_BUTTON);
+        FreeMem(wi, sizeof(*wi));
     }
-    return(FALSE);
+    return(NULL);
 }
 
-void freeImages(struct Image img[])
+void closeWindow(struct windowInfo *wi)
 {
-    WORD i;
-
-    for (i = 0; i < IMG_COUNT; i++)
-    {
-        freeImage(img + i);
-    }
-}
-
-void initText(struct IntuiText *text, STRPTR name)
-{
-    text->LeftEdge = 4;
-    text->TopEdge = 4;
-    text->NextText = NULL;
-    text->FrontPen = 4;
-    text->BackPen = 0;
-    text->DrawMode = JAM1;
-    text->ITextFont = NULL;
-    text->IText = name;
-}
-
-void initButton(struct Gadget *gad, struct IntuiText *text, WORD gid, WORD x, WORD y, struct Image *render, struct Image *select)
-{
-    gad->NextGadget = NULL;
-    gad->LeftEdge = x;
-    gad->TopEdge = y;
-    gad->Width = render->Width;
-    gad->Height = render->Height;
-    gad->Flags = GFLG_GADGIMAGE|GFLG_GADGHIMAGE;
-    gad->GadgetType = GTYP_BOOLGADGET;
-    gad->Activation = GACT_IMMEDIATE|GACT_RELVERIFY;
-    gad->GadgetRender = render;
-    gad->SelectRender = select;
-    gad->GadgetText = text;
-    gad->MutualExclude = 0;
-    gad->GadgetID = gid;
-    gad->UserData = NULL;
-    gad->SpecialInfo = NULL;
-}
-
-void initButtons(struct Gadget gad[], struct Image img[], struct IntuiText text[])
-{
-    WORD i;
-
-    initButton(gad + GID_MENU1, text + GID_MENU1, GID_MENU1, 0, 240, img + IMG_BUTTON, img + IMG_PRESSED);
-    initButton(gad + GID_MENU2, text + GID_MENU2, GID_MENU2, 64, 240, img + IMG_BUTTON, img + IMG_PRESSED);
-    initButton(gad + GID_MENU3, text + GID_MENU3, GID_MENU3, 128, 240, img + IMG_BUTTON, img + IMG_PRESSED);
-    initButton(gad + GID_MENU4, text + GID_MENU4, GID_MENU4, 192, 240, img + IMG_BUTTON, img + IMG_PRESSED);
-    initButton(gad + GID_MENU5, text + GID_MENU5, GID_MENU5, 256, 240, img + IMG_BUTTON, img + IMG_PRESSED);
-
-    gad[GID_MENU5].Activation |= GACT_TOGGLESELECT;
-
-    for (i = 0; i < GID_COUNT - 1; i++)
-    {
-        gad[i].NextGadget = gad + i + 1;
-    }
-}
-
-void initReqButtons(struct Gadget gad[], struct Image img[], struct IntuiText text[])
-{
-    WORD i;
-
-    initButton(gad + RID_CLOSE, NULL, RID_CLOSE, 0, 0, img + IMG_CLOSE, img + IMG_CLOSE_PRESSED);
-    initButton(gad + RID_OPT1, text + RID_OPT1, RID_OPT1, 32, 112, img + IMG_BUTTON, img + IMG_PRESSED);
-    initButton(gad + RID_OPT2, text + RID_OPT2, RID_OPT2, 96, 112, img + IMG_BUTTON, img + IMG_PRESSED);
-
-    for (i = 0; i < RID_COUNT - 1; i++)
-    {
-        gad[i].NextGadget = gad + i + 1;
-    }
-}
-
-void initTexts(struct IntuiText text[])
-{
-    initText(text + GID_MENU1, "Nowa gra");
-    initText(text + GID_MENU2, "Wczytaj grë");
-    initText(text + GID_MENU3, "Restartuj");
-    initText(text + GID_MENU4, "Poziom");
-    initText(text + GID_MENU5, "Opcje edycji");
-}
-
-void initEditTexts(struct IntuiText text[])
-{
-    initText(text + GID_MENU1, "Kafelek");
-    initText(text + GID_MENU2, "Odtwórz");
-    initText(text + GID_MENU3, "Zapisz");
-    initText(text + GID_MENU4, "Poziom");
-    initText(text + GID_MENU5, "Opcje edycji");
-}
-
-void initReqTexts(struct IntuiText text[], STRPTR opt1, STRPTR opt2)
-{
-    initText(text + RID_OPT1, opt1);
-    initText(text + RID_OPT2, opt2);
-}
-
-BOOL initWindow(struct windowInfo *wi, struct BitMap *gfx)
-{
-    if (initImages(wi->img, gfx))
-    {
-        initTexts(wi->text);
-        initButtons(wi->gads, wi->img, wi->text);
-        return(TRUE);
-    }
-    return(FALSE);
-}
-
-void freeWindow(struct windowInfo *wi)
-{
-    freeImages(wi->img);
-}
-
-void initReq(struct reqInfo *ri, struct windowInfo *wi, STRPTR opt1, STRPTR opt2)
-{
-    ri->img = wi->img;
-    initReqTexts(ri->text, opt1, opt2);
-    initReqButtons(ri->gads, ri->img, ri->text);
+    CloseWindow(wi->window);
+    FreeMem(wi, sizeof(*wi));
 }
